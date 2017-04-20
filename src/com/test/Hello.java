2b4d89a444c0f0d287e0e6f6965fa7d4f6d3441d
balueie <balueie@gmail.com>
2017-04-20 14:25:44 +0530

    Update Hello.java

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -1,7 +1,8 @@
 package com.test;
 
 public class Hello {
-
+String name= null;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Hello World");
